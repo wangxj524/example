b02dfd99bb80d7fc32ad5ba34ebb6f9c94c9d906
Jerry Wang <wxj524@163.com>
2018-09-14 15:59:40 +0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# _*_coding:utf-8_*_
+"""
+@Date:   2018/9/13
+@Author: WangXJ
+===============================================================================
+Intro:
+===============================================================================
+"""
+
+# import modules
+from scrapy import cmdline
+
+
+cmdline.execute(["scrapy", "crawl", "BookSpider"])
